cede817c5ddbb6ec405af61ac3f548c78abccb4c
SuciuCalin <e.suciu.c@gmail.com>
2019-10-26 13:06:57 +0300

    comments

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-skins
-mai mute feuri de prajituri
-
-
-buyable or achievable
-buy boost for clicks
-
-
-
-referinte cand tii cu mouseu pe un ob
-achiev
-don t stop eating/clcking
-no lifer
-madlad   pentru ??? clickuri pe 5 10 secunde
-pt putine clickuri ... lighter fighter/ light cookie
-
-dupa ce resetezi p0ti face m mute ucruri si mut mai us0r
+ - [ ] skins
+ - [ ] mai mute feuri de prajituri
+ - [ ] buyable or achievable
+ - [ ] buy boost for clicks
+ - [ ] referinte cand tii cu mouseu pe un ob
+ - [ ] achiev
+ - [ ] don t stop eating/clcking
+ - [ ] no lifer
+ - [ ] madlad   pentru ??? clickuri pe 5 10 secunde
+ - [ ] pt putine clickuri ... lighter fighter/ light cookie
+ - [ ] dupa ce resetezi poti face mumute lucruri si mult mai usor
--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+// const clickingArea - Variabila JavaScript
+// document.getElementById() - selecteaza elementul din HTML cu id-ul clickingArea
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+// document.getElementsByClassName() - selecteaza elementul din HTML dupa clasa increment-click-value
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
+// https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+// https://www.w3schools.com/jsref/prop_element_classlist.asp
 const clickingArea = document.getElementById('clickingArea')
 const counter = document.getElementById('counter')
 const buttonIncrementClickValue = document.getElementsByClassName('increment-click-value')[0]
