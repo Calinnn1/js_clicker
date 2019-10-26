533de31ce88643ca04042018765140d83cf0943b
SuciuCalin <e.suciu.c@gmail.com>
2019-10-26 11:59:48 +0300

    link javascript with html

--- js/index.js
+++ js/index.js
@@ -2,20 +2,18 @@ function Counter(valor) {
 
     var ClickerButton = 0;
     ClickerButton += 1;
-    if (valor >== 20); {
+    if (valor >= 20) {
         return IncrementClickValue;
-    } else {
-        (valor >== 50);
+    } else if (valor >= 50) {
         return IdleClick;
     }
-    
     document.getElementById("number").innerHTML = ClickerButton;
 }
 
 
 
 
-/* var 
+/* var
 if (ClickerButton >= 20) {
     return IncrementClickValue;
 } else (ClickerButton >= 50); {
